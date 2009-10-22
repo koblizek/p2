8c890b719c16599789982f6502ef63e86c9202ee
Robert Koblizek <koblizek@volny.cz>
2009-10-22 16:00:55 +0200

    první zavedení

---
+++ app/controllers/application.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+  protect_from_forgery # :secret => 'c069ec6fe69a4aaf108e2642541d1f73'
+  
+  puts '## AplicationController P3' 
+  
+  
+end
